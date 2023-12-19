8bf53aa744f1c1328293ac49afc5be1d9b0750bb
Duarte Silva <duartemcsilva@gmail.com>
2023-12-19 12:03:48 +0000

    Added icons to enhance ui

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,6 @@
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 import Button from '../ui/button';
 import classes from './event-item.module.css';
 
@@ -19,14 +22,17 @@ export default function EventItem( { event }) {
                             {event.title}
                         </h2>
                         <div className={classes.date}>
+                            <DateIcon/>
                             <time>{readableDate}</time>
                         </div>
                         <div className={classes.address}>
+                            <AddressIcon/>
                             <address>{formattedAddress}</address>
                         </div>
                     </div>
                     <div className={classes.actions}>
-                        <Button link={exploreLink}>Explore Event</Button>
+                        <Button link={exploreLink}><span>Explore Event</span> 
+                        <span className={classes.icon}><ArrowRightIcon/></span></Button>
                     </div>
                 </div>
             </li>
